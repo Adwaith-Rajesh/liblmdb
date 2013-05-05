Adwaith-Rajesh/liblmdb

commit 2d75bc6791662444423a1c961c6ef318a81aa40d

Plug MDB memory leak of DB names.

mdb.c
@@ -3707,9 +3707,13 @@ void
 mdb_env_close(MDB_env *env)
 {
 	MDB_page *dp;
+	int i;
 
 	if (env == NULL)
 		return;
+
+	for (i = env->me_numdbs; --i > MAIN_DBI; )
+		free(env->me_dbxs[i].md_name.mv_data);
 
 	VGMEMP_DESTROY(env);
 	while ((dp = env->me_dpages) != NULL) {

commit a2ee236669f00f9afe8d89c8f02dd6b922231502

Fix mdb_env_close(unopened MDB_env).
Do not try to scan me_dbxs in a closed/never-opened MDB_env.
Broken by 2d75bc6791662444423a1c961c6ef318a81aa40d
and f351fcbde38e366c78b1fdb2760deb07023531ad.

mdb.c
@@ -3548,7 +3548,7 @@ leave:
 	return rc;
 }
 
-/** Destroy resources from mdb_env_open() and clear our readers */
+/** Destroy resources from mdb_env_open(), clear our readers & DBIs */
 static void
 mdb_env_close0(MDB_env *env, int excl)
 {
@@ -3556,6 +3556,10 @@ mdb_env_close0(MDB_env *env, int excl)
 
 	if (!(env->me_flags & MDB_ENV_ACTIVE))
 		return;
+
+	/* Doing this here since me_dbxs may not exist during mdb_env_close */
+	for (i = env->me_maxdbs; --i > MAIN_DBI; )
+		free(env->me_dbxs[i].md_name.mv_data);
 
 	free(env->me_dbflags);
 	free(env->me_dbxs);
@@ -3764,13 +3768,9 @@ void
 mdb_env_close(MDB_env *env)
 {
 	MDB_page *dp;
-	int i;
 
 	if (env == NULL)
 		return;
-
-	for (i = env->me_maxdbs; --i > MAIN_DBI; )
-		free(env->me_dbxs[i].md_name.mv_data);
 
 	VGMEMP_DESTROY(env);
 	while ((dp = env->me_dpages) != NULL) {